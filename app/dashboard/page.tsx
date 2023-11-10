edde16a905ddeb2275d0473235174eaf8affb5bc
DemaPy <benderdema@gmail.com>
2023-11-10 14:35:04 +0100

    second commit

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../ui/global.css";
+
+type Props = {}
+
+const page = (props: Props) => {
+  return (
+    <div>Dashboard page</div>
+  )
+}
+
+export default page
